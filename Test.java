fc6f2baab0c5ad65989fbcef39315f2bf965a3ef
ainugiri12 <ainugiri@gmail.com>
2023-06-20 11:56:35 +0530

    Added TestJava File

---
+++ Test.java
@@ -0,0 +1,12 @@
+class Test{
+public static void main(String[] args){
+	String s1 = new String("ATOS");
+	String s2 = new String("ATOS");
+	String s3 = "Eviden";
+	String s4 = "Eviden";
+	System.out.println(s1);
+	System.out.println(s2);
+	System.out.println(s3);
+	System.out.println(s4);
+}
+}
